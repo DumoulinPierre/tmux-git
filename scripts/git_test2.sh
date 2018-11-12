7a347f2eee21a13d361071b420fa15b7393065be
Pierre Dumoulin <pierredumoulin@MacBook-Pro-de-Pierre.local>
2018-11-12 22:56:37 +0100

    WIP

--- scripts/git_test2.sh
+++ scripts/git_test2.sh
@@ -5,6 +5,6 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$CURRENT_DIR/helpers.sh"
 
 main() {
-	print_test2
+  tmux showenv -g TMUX_PWD_$(tmux display -p "#D" | tr -d %)  | sed 's/^.*=//'
 }
 main
